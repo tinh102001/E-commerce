5cce504b472d9d21337ea53660724497833c02d3
tinh102001 <tinhduc312001@gmail.com>
2022-09-10 22:53:09 +0700

    sever config

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const URI = process.env.DATABASE_URL;
+
+app.use(cors());
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
+
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('err', err);
+  });

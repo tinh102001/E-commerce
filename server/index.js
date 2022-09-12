6c0e998cc5c34f1be003da501836b5c93a522786
tinh102001 <tinhduc312001@gmail.com>
2022-09-12 22:00:54 +0700

    Update API upload

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 import user from "./routers/user.router.js";
 import category from "./routers/category.router.js";
+import upload from "./routers/upload.router.js";
 
 dotenv.config();
 
@@ -18,10 +20,12 @@ app.use(cors());
 app.use(express.json({ limit: "30mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
+app.use(fileUpload({ useTempFiles: true }));
 
 // API
 app.use("/user", user);
 app.use("/api", category);
+app.use("/api", upload);
 
 mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
